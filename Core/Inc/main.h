a5e3356c734f021d53ee76bd505dca8c3baa4391
Lazo Attar <green4applez@gmail.com>
2023-02-05 17:13:25 -0500

    Remap GPS pins

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -12,12 +12,12 @@ void DMA_Init(void);
 
 void USART3_DMA1_Stream3_Write(volatile uint8_t *data, uint16_t length);
 void USART3_DMA1_Stream1_Read(volatile uint8_t *buffer, uint16_t length);
-void UART8_DMA1_Stream4_Write(volatile uint8_t *data, uint16_t length);
-void UART8_DMA1_Stream0_Read(volatile uint8_t *buffer, uint16_t length);
+void UART9_DMA1_Stream4_Write(volatile uint8_t *data, uint16_t length);
+void UART9_DMA1_Stream0_Read(volatile uint8_t *buffer, uint16_t length);
 void UART7_DMA1_Stream2_Read(volatile uint8_t *buffer, uint16_t length);
 
 uint8_t Is_USART3_Buffer_Full(void);
-uint8_t Is_UART8_Buffer_Full(void);
+uint8_t Is_UART9_Buffer_Full(void);
 uint8_t Is_UART7_Buffer_Full(void);
 
 __attribute__ ((section(".buffer"))) extern volatile uint8_t uart8_rx_data[];
@@ -29,8 +29,8 @@ __attribute__ ((section(".buffer"))) extern volatile uint8_t usart3_tx_data[];
 
 extern volatile uint8_t usart3_tx_finished;
 extern volatile uint8_t usart3_rx_finished;
-extern volatile uint8_t uart8_tx_finished;
-extern volatile uint8_t uart8_rx_finished;
+extern volatile uint8_t uart9_tx_finished;
+extern volatile uint8_t uart9_rx_finished;
 extern volatile uint8_t uart7_tx_finished;
 extern volatile uint8_t uart7_rx_finished;
 
