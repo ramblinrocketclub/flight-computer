783cc53a4e8c3b289fa21798c9396633ce7c9544
Nicolas Salischiker <nico.salis@icloud.com>
2023-02-20 15:21:51 -0500

    Implemented apogee detection

--- Core/Src/states/coast2.c
+++ Core/Src/states/coast2.c
@@ -2,16 +2,36 @@
 
 #include "states.h"
 
+double averageVelocity = 0;
+double count = 0;
+int sampleSize = APOGEE_CHECK_FREQ * APOGEE_CHECK_TIME_SEC;
+
 void coast2_initialize(double currentTimestampSec) {
     printf("Entered coast2 state");
+    rocket.velocity_check_time_sec = currentTimestampSec;
 }
 
 State *coast2_execute(double currentTimestampSec) {
-    //move on to recovery if apogee detected (average negative velocity for five seconds) OR max chute deploy time reached
-    //TODO: apogee detection
-    /*if ( || rocket.fsv.time_since_launch_seconds > CHUTE_DEPLOY_TIME_SEC) {
+    //move on to recovery if apogee detected   
+    if (currentTimestampSec >= rocket.velocity_check_time_sec + APOGEE_CHECK_FREQ) {
+        averageVelocity += get_vertical_vel_msec(&rocket.fsv) / sampleSize;
+        count++;
+        //if 
+        if (count >= sampleSize) {
+            if (averageVelocity < 0) {
+                return &recovery;
+            }
+            averageVelocity = 0;
+            count = 0;
+            rocket.velocity_check_time_sec = currentTimestampSec;
+        }
+    }
+
+    //move on to recovery if max chute deploy time reached
+    if (rocket.fsv.time_since_launch_seconds > CHUTE_DEPLOY_TIME_SEC) {
         return &recovery;
-    }*/
+    }
+
     return &coast2;
 }
 
